4b15e61957d2b38f5c47adb2471eecdf82f8c598
Praxal <praxalshah@gmail.com>
2025-07-02 17:17:11 -0400

    feat(backend): Scaffold Spring Boot application

---
+++ backend/src/test/java/com/kitchent/api/MealPlannerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kitchent.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealPlannerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
